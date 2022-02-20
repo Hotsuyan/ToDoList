6fd81cbf564b654f898fe0f15561993edc03c911
Hotsuyan <pelliccia.gioele@gmail.com>
2022-02-20 16:40:29 +0100

    Added the bared skeleton of the page, it doesn't work yet

---
+++ javascript.js
@@ -0,0 +1,15 @@
+const form = document.querySelector("form");
+const input = document.querySelector("[name='todo']")
+const toDoList = document.getElementById('todos');
+
+const toDoData = [];
+
+function addToDo (text) {
+    toDoData.push(text);
+    const li = document.createElement("li");
+    li.innerHTML = text;
+    toDoList.appendChild(li);
+
+}
+form.onsubmit = (e)=> addToDo(input.value);
+    
